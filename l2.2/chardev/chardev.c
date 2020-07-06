0be92b27ef1b73498cfa4393c038c85145085e1c
turbq <gron069@gmail.com>
2020-07-06 15:37:42 +0000

    chardev successfully opens

--- l2.2/chardev/chardev.c
+++ l2.2/chardev/chardev.c
@@ -8,24 +8,77 @@
 static char *kbuf;
 static dev_t first;
 static unsigned int count = 1;
-static int my_major = 700, my_mnor = 0;
+static int my_major = 700, my_minor = 0;
 static struct cdev *my_cdev;
 
 #define MYDEV_NAME "mychardev"
-#define KBUF_SIZE size_t ((10) * PAGE_SIZE)
+#define KBUF_SIZE (size_t) ((10) * PAGE_SIZE)
 
-static int __init init_module(void)
+static int mychrdev_open(struct inode *inode, struct file *file)
 {
-    printk( KERN_INFO "Hello, loading" );
+    printk(KERN_INFO "opening device %s:\n\n", MYDEV_NAME);
     return 0;
 }
 
-static void __exit cleanup_module(void)
+static int mychrdev_release (struct inode *inode, struct file *file)
+{
+    printk(KERN_INFO "chardev release %s:\n\n", MYDEV_NAME);
+    return 0;
+}
+
+static ssize_t mychrdev_read(struct file *file, char __user *buf, size_t lbuf, loff_t *ppos)
+{
+    int nbytes = lbuf - copy_to_user(buf, kbuf + *ppos, lbuf);
+    *ppos += nbytes;
+    
+    printk(KERN_INFO "read device %s nbytes = %d, ppos = %d:\n\n",
+            MYDEV_NAME, nbytes, (int) *ppos);
+
+    return nbytes;
+}
+
+static ssize_t mychrdev_write(struct file * file, const char __user *buf,
+        size_t lbuf, loff_t *ppos)
+{
+    int nbytes = lbuf - copy_from_user(kbuf + *ppos, buf, lbuf);
+    *ppos += nbytes;
+
+    printk(KERN_INFO "write device %s nbytes = %d ppos = %d:\n\n", MYDEV_NAME,
+            nbytes, (int) *ppos);
+
+    return nbytes;
+}
+
+static const struct file_operations mycdev_fops = {
+    .owner = THIS_MODULE,
+    .read = mychrdev_read,
+    .write = mychrdev_write,
+    .open = mychrdev_open,
+    .release = mychrdev_release
+};
+
+static int __init init_chardev(void)
+{
+    printk( KERN_INFO "Hello, loading" );
+    kbuf = kmalloc(KBUF_SIZE, GFP_KERNEL);
+    
+    first = MKDEV (my_major, my_minor);
+    register_chrdev_region (first,count,MYDEV_NAME);
+
+    my_cdev = cdev_alloc();
+
+    cdev_init(my_cdev, &mycdev_fops);
+    cdev_add(my_cdev, first, count);
+
+    return 0;
+}
+
+static void __exit cleanup_chardev(void)
 {
     printk( KERN_INFO "Leaving" );
 }
 
 module_init (init_chardev);
-module_exit (exit_chardev);
+module_exit (cleanup_chardev);
 
 MODULE_LICENSE("GPL");
